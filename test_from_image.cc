d4d7f66b4fdf6fb601b49474200e6544d8d414f2
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2017-09-07 17:11:00 -0700

    trivial update

--- test_from_image.cc
+++ test_from_image.cc
@@ -44,7 +44,7 @@ int main(int argc, char* argv[])
 {
     if( argc != 2 )
     {
-        fprintf(stderr, "missing arg: need image filename on the cmdline");
+        fprintf(stderr, "missing arg: need image filename on the cmdline\n");
         return 1;
     }
 
