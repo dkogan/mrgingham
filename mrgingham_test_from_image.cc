b0856c7385ebc49c29107f0d4e27deb3af75dda0
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2017-10-31 16:39:28 -0700

    mrgingham_test_from_image can be asked to work with a particular image level

--- mrgingham_test_from_image.cc
+++ mrgingham_test_from_image.cc
@@ -11,31 +11,39 @@ using namespace mrgingham;
 int main(int argc, char* argv[])
 {
     const char* usage =
-        "Usage: %s [--clahe] [--blur radius] --blobs|--chessboard imagefile\n"
+        "Usage: %s [--clahe] [--blur radius] [--level l] --blobs|--chessboard imagefile\n"
         "\n"
         "  --blobs or --chessboard are required; these tell the tool what to do.\n"
         "\n"
         "  --clahe is optional: it will pre-process the image with an adaptive histogram\n"
         "  equalization step. This is useful if the calibration board has a lighting\n"
-        "  gradient across it."
+        "  gradient across it.\n"
         "\n"
         "  --blur radius   applies a blur (after --clahe, if given) to the image before\n"
-        "  processing\n";
+        "  processing\n"
+        "\n"
+
+        "  --level l   applies a downsampling to the image before processing it (after\n"
+        "  --clahe and --blur, if given) to the image before processing. Level 0 means\n"
+        "  'use the original image'. Level > 0 means downsample by 2**level. Level < 0\n"
+        "  means 'try several different levels until we find one that works'.\n";
 
     struct option opts[] = {
         { "blobs",      no_argument,       NULL, 'B' },
         { "chessboard", no_argument,       NULL, 'C' },
         { "blur",       required_argument, NULL, 'b' },
         { "clahe",      no_argument,       NULL, 'c' },
+        { "level",      required_argument, NULL, 'l' },
         { "help",       no_argument,       NULL, 'h' },
         {}
     };
 
 
-    bool have_doblobs = false;
-    bool doblobs      = false; // to pacify compiler
-    bool doclahe      = false;
-    int  blur_radius  = -1;
+    bool have_doblobs        = false;
+    bool doblobs             = false; // to pacify compiler
+    bool doclahe             = false;
+    int  blur_radius         = -1;
+    int  image_pyramid_level = -1;
 
     int opt;
     do
@@ -77,6 +85,10 @@ int main(int argc, char* argv[])
             }
             break;
 
+        case 'l':
+            image_pyramid_level = atoi(optarg);
+            break;
+
         case '?':
             fprintf(stderr, usage, argv[0]);
             return 1;
@@ -87,6 +99,11 @@ int main(int argc, char* argv[])
     {
         fprintf(stderr, usage, argv[0]);
         return 1;
+    }
+
+    if( doblobs && image_pyramid_level >= 0)
+    {
+        fprintf(stderr, "warning: 'image_pyramid_level' only implemented for chessboards. Will be ignored\n");
     }
 
     const char* filename = argv[argc-1];
@@ -114,7 +131,7 @@ int main(int argc, char* argv[])
     if(doblobs)
         result = find_circle_grid_from_image_array(points_out, image);
     else
-        result = find_chessboard_from_image_array (points_out, image);
+        result = find_chessboard_from_image_array (points_out, image, image_pyramid_level);
 
     if( result )
     {
