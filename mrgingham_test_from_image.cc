48cc82a6cf805ee4de330cf5f030917ecdeb80c1
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2017-10-31 15:39:35 -0700

    reworked option parsing in mrgingham_test_from_image

--- mrgingham_test_from_image.cc
+++ mrgingham_test_from_image.cc
@@ -1,35 +1,69 @@
 #include "mrgingham.hh"
 #include <stdio.h>
+#include <getopt.h>
 
 using namespace mrgingham;
 
 int main(int argc, char* argv[])
 {
-    const char* usage = "Usage: %s blobs|chessboard imagefile\n";
+    const char* usage = "Usage: %s --blobs|--chessboard imagefile\n";
 
-    if( argc != 3 )
+    struct option opts[] = {
+        { "blobs",      no_argument, NULL, 'B' },
+        { "chessboard", no_argument, NULL, 'C' },
+        { "help",       no_argument, NULL, 'h' },
+        {}
+    };
+
+
+    bool have_doblobs = false;
+    bool doblobs      = false; // to pacify compiler
+
+    int opt;
+    do
+    {
+        // "h" means -h does something
+        opt = getopt_long(argc, argv, "h", opts, NULL);
+        switch(opt)
+        {
+        case -1:
+            break;
+
+        case 'h':
+            printf(usage, argv[0]);
+            return 0;
+
+        case 'C':
+            if( have_doblobs )
+            {
+                fprintf(stderr, usage, argv[0]);
+                return 1;
+
+            }
+            have_doblobs = true;
+            doblobs      = (opt == 'B');
+            break;
+
+        case '?':
+            fprintf(stderr, usage, argv[0]);
+            return 1;
+        }
+    }  while( opt != -1 );
+
+    if( !have_doblobs || optind != argc-1)
     {
         fprintf(stderr, usage, argv[0]);
         return 1;
     }
 
-    bool doblobs;
-    if(      strcmp(argv[1], "blobs") == 0 )
-        doblobs = true;
-    else if( strcmp(argv[1], "chessboard") == 0 )
-        doblobs = false;
-    else
-    {
-        fprintf(stderr, usage, argv[0]);
-        return 1;
-    }
+    const char* filename = argv[argc-1];
 
     std::vector<PointDouble> points_out;
     bool result;
     if(doblobs)
-        result = find_circle_grid_from_image_file(points_out, argv[2]);
+        result = find_circle_grid_from_image_file(points_out, filename);
     else
-        result = find_chessboard_from_image_file (points_out, argv[2]);
+        result = find_chessboard_from_image_file (points_out, filename);
 
     if( result )
     {
