6a96e842abb69e2cc1d184d10b3044e6d5b1f9e8
Dima Kogan <dima@secretsauce.net>
2023-06-29 13:04:34 -0700

    new choose_mrbuild.mk

--- choose_mrbuild.mk
+++ choose_mrbuild.mk
@@ -3,15 +3,18 @@
 
 ifneq (,$(wildcard mrbuild/))
   MRBUILD_MK=mrbuild
-  MRBUILD_BIN=mrbuild
+  MRBUILD_BIN=mrbuild/bin
 else ifneq (,$(wildcard /usr/include/mrbuild/Makefile.common.header))
   MRBUILD_MK=/usr/include/mrbuild
   MRBUILD_BIN=/usr/bin
 else
-  MRBUILD_VER := 1.3
-  URL         := https://github.com/dkogan/mrbuild/archive/refs/tags/v${MRBUILD_VER}.tar.gz
+  V      := 1.5
+  SHA512 := 8b322fa41351d6b7a3f43c1e05929e4dbe4630f0bde3c9a1b7d6e3138ef8749853afa49cc800ff1c64453ac203e1128cf1ddbbda829100a6d1aeb3eb9f6fb7b0
 
-  cmd := wget -O v${MRBUILD_VER}.tar.gz ${URL} && sha512sum --ignore-missing --quiet --strict -c mrbuild.checksums && tar xvfz v${MRBUILD_VER}.tar.gz && mv mrbuild-${MRBUILD_VER} mrbuild
+  URL   := https://github.com/dkogan/mrbuild/archive/refs/tags/v$V.tar.gz
+  TARGZ := mrbuild-$V.tar.gz
+
+  cmd := wget -O $(TARGZ) ${URL} && sha512sum --quiet --strict -c <(echo $(SHA512) $(TARGZ)) && tar xvfz $(TARGZ) && ln -fs mrbuild-$V mrbuild
 
   $(error mrbuild not found. Either 'apt install mrbuild', or if not possible, get it locally like this: '${cmd}')
 endif
