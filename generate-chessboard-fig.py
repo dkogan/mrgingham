1d67ae6e39707b543a4c8f64caa212a90df47ef6
Dima Kogan <dima@secretsauce.net>
2023-08-31 10:24:23 -0700

    generate-chessboard-fig.py can produce a grid of circles

--- generate-chessboard-fig.py
+++ generate-chessboard-fig.py
@@ -21,6 +21,17 @@ def parse_args():
         argparse.ArgumentParser(description = __doc__,
                                 formatter_class=argparse.RawDescriptionHelpFormatter)
 
+    parser.add_argument('--blobs',
+                        action = 'store_true',
+                        help='''If given, we generate a grid of circles, not a
+                        chessboard''')
+    parser.add_argument('--circle-radius-cells',
+                        type=float,
+                        default = 0.2,
+                        help='''Applies only if --blobs. Specifies the radius of
+                        each circle, in the units of grid spacing: radius == 0.5
+                        would create circles that are large enough such that
+                        adjacent ones touch.''')
     parser.add_argument('Wcorners',
                         type=int,
                         help='''The number of chessboard corners in the
@@ -57,8 +68,9 @@ Single
 -2
 1200 2'''
 
-# arbitrary units
-square_size = 100
+# Arbitrary units. fig2dev appears to compute the bounding box in integer units,
+# so if this is too small, the bounding box will be off
+square_size = 10000
 
 def black_cell(x,y, *,
                double_width  = False,
@@ -76,11 +88,16 @@ def black_cell(x,y, *,
     print("2 2 0 0 0 0 50 -1 20 0.000 0 0 -1 0 0 5")
     print(f"  {X0} {Y0} {X1} {Y0} {X1} {Y1} {X0} {Y1} {X0} {Y0}")
 
-if (args.Wcorners // 2) * 2 != args.Wcorners or \
-   (args.Hcorners // 2) * 2 != args.Hcorners:
-    print("Wcorners and Hcorners are assumed to be even",
-          file = sys.stderr)
-    sys.exit(1)
+def black_circle(x,y,R):
+
+    '''Units of all x,y is "cells". Units of R is "fig units"'''
+
+    X = x * square_size
+    Y = y * square_size
+
+    print(f"1 3 0 0 0 0 50 -1 20 0 1 0 {X} {Y} {R} {R} {X} {Y} {X+R} {Y}")
+
+
 
 
 Wcells = args.Wcorners + 3
@@ -88,22 +105,37 @@ Hcells = args.Hcorners + 3
 
 print(header)
 
-# top/bottom edges
-for y in (0,Hcells-2):
-    for x in range(2,Wcells-2,2):
-        black_cell(x,y,
-                   double_height = True)
+if not args.blobs:
 
-# left/right edges
-for x in (0,Wcells-2):
+    if (args.Wcorners // 2) * 2 != args.Wcorners or \
+       (args.Hcorners // 2) * 2 != args.Hcorners:
+        print("Wcorners and Hcorners are assumed to be even",
+              file = sys.stderr)
+        sys.exit(1)
+
+    # top/bottom edges
+    for y in (0,Hcells-2):
+        for x in range(2,Wcells-2,2):
+            black_cell(x,y,
+                       double_height = True)
+
+    # left/right edges
+    for x in (0,Wcells-2):
+        for y in range(2,Hcells-2,2):
+            black_cell(x,y,
+                  double_width = True)
+
+    # middle
     for y in range(2,Hcells-2,2):
-        black_cell(x,y,
-              double_width = True)
+        for x in range(3,Wcells-2,2):
+            black_cell(x,y)
+    for y in range(3,Hcells-2,2):
+        for x in range(2,Wcells-2,2):
+            black_cell(x,y)
+else:
 
-# middle
-for y in range(2,Hcells-2,2):
-    for x in range(3,Wcells-2,2):
-        black_cell(x,y)
-for y in range(3,Hcells-2,2):
-    for x in range(2,Wcells-2,2):
-        black_cell(x,y)
+    R = int(round(square_size * args.circle_radius_cells))
+
+    for y in range(0,args.Hcorners):
+        for x in range(0,args.Wcorners):
+            black_circle(x,y,R)
